f3c67e4bb52425f8e12387b7254bd2a0a3ea68ec
AJITH19233 <135130218+AJITH19233@users.noreply.github.com>
2023-11-04 16:34:28 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+var id=document.getElementById("user");
+var pass=document.getElementById("password");
+var btn=document.getElementById("btn1");
+btn.addEventListener('click',(e)=>{
+    verify(id,pass,(message)=>{
+        alert(message);
+    })
+})
+let verify=(id,pass,callback)=>{
+    if(id.value==""||pass.value==""){
+        callback("Kindly fill the form");
+    }else if(id.value=="admin"&&pass.value=="12345"){
+        window.location.href="index.html";
+        callback("credential is correct!!");
+
+    }
+    else{
+        alert("invalid Credential");
+    }
+}
---
+++ todolist.js
@@ -0,0 +1,31 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        var tableBody = document.getElementById("data-body");
+        var completedMessage = document.getElementById("completed-message");
+        var completedCount = 0;
+
+        for (var i = 0; i < data.length; i++) {
+            var row = tableBody.insertRow(i);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = data[i].title
+
+            var checkbox = document.createElement("input");
+                    checkbox.type = "checkbox";
+                    checkbox.addEventListener("change", function() {
+                        if (this.checked) {
+                            completedCount++;
+                            if (completedCount >= 5) {
+                                alert("Congratulation!!!Your's 5 Task are completed.");
+                            }
+                        }
+                    });
+                    cell2.appendChild(checkbox);
+                }
+            }
+        };
+
+        xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+        xhttp.send();
